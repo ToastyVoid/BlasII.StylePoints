c36c47768430d17f27c2dfa6725e374e9f3aa761
ToastyVoid <bonlonsk@myyahoo.com>
2026-05-28 10:54:58 +0530

    Update RatingsManager.cs

--- BlasII.StylePoints/Ratings/RatingsManager.cs
+++ BlasII.StylePoints/Ratings/RatingsManager.cs
@@ -39,13 +39,14 @@ public class RatingsManager : Manager<RatingID, Rating>
 	/// </summary>
 	protected override void Fill()
 	{
-		Add(RatingID.D, "Desecrator", Color.cyan, 1, null, RatingID.C);
-		Add(RatingID.C, "Corrupt", Color.green, 50, RatingID.D, RatingID.B);
-		Add(RatingID.B, "Blasphem", Color.yellow, 150, RatingID.C, RatingID.A);
-		Add(RatingID.A, "Apostate", new Color(0.75f, 0.5f, 0f, 1f), 300, RatingID.B, RatingID.S);
-		Add(RatingID.S, "Sacreligious", Color.red, 500, RatingID.A, RatingID.SS);
-		Add(RatingID.SS, "Qwasker", Color.red, 1000, RatingID.S, RatingID.SSS);
-		Add(RatingID.SSS, "Blasphemous II", Color.red, 2000, RatingID.SS, null);
+	// I know what i am doing :D
+		Add(RatingID.D, "Eviterno", Color.cyan, 1, null, RatingID.C);
+		Add(RatingID.C, "Markus", Color.green, 50, RatingID.D, RatingID.B);
+		Add(RatingID.B, "Oisin", Color.yellow, 150, RatingID.C, RatingID.A);
+		Add(RatingID.A, "Anddessine", new Color(0.75f, 0.5f, 0f, 1f), 300, RatingID.B, RatingID.S);
+		Add(RatingID.S, "Druh", Color.red, 500, RatingID.A, RatingID.SS);
+		Add(RatingID.SS, "Qwasker", Color.red, 1000, RatingID.S, RatingID.SSS); // LMAO :D
+		Add(RatingID.SSS, "Shadow Moses", Color.red, 2000, RatingID.SS, null);
 	}
 }
 
